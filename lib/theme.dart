dc471c714aa4c4c01f4800f2a46746bfa13b5fcb
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2021-05-15 01:55:44 +0700

    add sign in page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = Color(0xff6C5ECF);
 Color secondaryColor = Color(0xff38ABBE);
 Color alertColor = Color(0xffED6363);
@@ -10,6 +12,7 @@ Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
+Color subtitleColor = Color(0xff504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -19,8 +22,16 @@ TextStyle secondaryTextStyle = GoogleFonts.poppins(
   color: secondaryTextColor,
 );
 
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
 );
 
 FontWeight light = FontWeight.w300;
